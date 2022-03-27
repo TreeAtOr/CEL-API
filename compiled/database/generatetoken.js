79de8d0493d6e627e5df7a242008daf277f16828
TreeAtOr <gdorevino@gmail.com>
2022-03-27 18:09:43 +0300

    Remove compiled JS code from repo

--- compiled/database/generatetoken.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateToken = void 0;
-function generateToken(_id, length) {
-    var memoryble = false;
-    return ("GenerateToken function is not implemented.");
-}
-exports.generateToken = generateToken;
